3b033fcca00e644b7ed2f749dc20501f0bdc1c4f
David King <dave@davbo.org>
2013-12-19 16:40:26 +0000

    Added test script for making a request with curl and openssl

---
+++ auth_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+method=$1
+url=$2
+apikey=$3
+secret=$4
+
+date=$(date)
+nonce=$((RANDOM))
+
+canonical_form="$method
+$url
+date:$date
+x-hmac-nonce:$nonce"
+
+echo "Canonical form:"
+echo $canonical_form
+
+authorize=$(echo -n "$canonical_form" | openssl sha1 -hmac "$secret" | sed 's/^.* //')
+
+echo "Authorize header:"
+echo $authorize
+
+echo $(curl -v -H "X-Moxie-Key: $apikey" -H "Date: $date" -H "X-HMAC-Nonce: $nonce" -H "Authorization: $authorize" -X $method $url)
